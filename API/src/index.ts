61c5f8d29f4ca8225b5a49766f3176da356c7dcf
Thiago Ogawa <thiago.ogawa11@gmail.com>
2026-02-11 22:56:15 -0300

    chore: set up development environment with Express, Nodemon and ts-node

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,2 +1,14 @@
+import express from 'express';
 
+const app = express();
+const port = 3001;
+
+app.get('/', (_, res) => {
+  console.log('REQ RECEBIDA');
+  res.send('API funcionando 🚀');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`⚙️ Server running → http://localhost:${port}`);
+});
 
