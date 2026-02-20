9124bd8164523d2102151dfc91023c1863efc61c
Thiago Ogawa <thiago.ogawa11@gmail.com>
2026-02-20 16:31:22 -0300

    feat: implement error handling for listCategories &  implement listProducts

--- API/src/app/router.ts
+++ API/src/app/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { listCategories } from './useCases/categories/listCategories.js';
 import { createCategory } from './useCases/categories/createCategory.js';
+import { listProducts } from './useCases/products/listProducts.js';
 
 export const router = Router();
 
@@ -12,9 +13,7 @@ router.get('/categories', listCategories);
 router.post('/categories', createCategory);
 
 // List products
-router.get('/products', (req, res) => {
-  res.send('OK');
-});
+router.get('/products', listProducts);
 
 // Create product
 router.post('/products', (req, res) => {
