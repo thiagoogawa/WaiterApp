3cf525df8adcda903f971a65872bfd266a8dd561
Thiago Ogawa <thiago.ogawa11@gmail.com>
2026-02-26 11:02:34 -0300

    chore: enable express.static for uploads folder

--- API/src/app/index.ts
+++ API/src/app/index.ts
@@ -1,13 +1,19 @@
+import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
+import { fileURLToPath } from 'url';
 
 import { router } from './router.js';
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://localhost:27017/waiterapp')
   .then(() => {
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
     const app = express();
     const port = 3001;
 
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', '..', 'uploads')));
     app.use(express.json());
     app.use(router);
 
@@ -15,4 +21,4 @@ mongoose.connect('mongodb://localhost:27017')
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
-  .catch(() => console.log('Erro ao conectar no mongodb'));
+  .catch((err) => console.error('Erro ao conectar no mongodb:', err));
